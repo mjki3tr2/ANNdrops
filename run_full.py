d2bf0fe3fee5c38b1951cb4bac6539cee2706d7a
Tom Rodgers <tom.rodgers@manchester.ac.uk>
2025-05-24 14:16:25 +0100

    Correction of run_full.py for scaling
    Corrected error in re-scaling the output of the model when all the data is used.

--- run_full.py
+++ run_full.py
@@ -34,9 +34,9 @@ validate_to_train_ratio = 0.3
 #'threshold_activation' - threshold of 0.1 min
 #'normalized_relu' - make sure the sum is 1 by normalisation
 
-n_runs = 1 # minimisation loops
-num_optimise = 2 # number of minimisaion steps
-num_initial = 1 # number of initial mapping guesses
+n_runs = 5 # minimisation loops
+num_optimise = 70 # number of minimisaion steps
+num_initial = 20 # number of initial mapping guesses
 space_model = [
     Real(1e-5, 1e-2,name='lr', prior='log-uniform'),
     Integer(500, 1000, name='epochs'),
@@ -51,7 +51,7 @@ space_model = [
 Initial Setup
 """
 # read in the data
-DSD, diameter, properties, labels = read_data('Data_Summary.csv')
+DSD, diameter, properties, labels = read_data('Data_Summary2.csv')
 fits, fit_labels = read_fits('Data_fits.csv')
 
 # calculate the d32's of the data
@@ -60,8 +60,7 @@ for index in range(len(DSD)):
     d32_exp[index] = calc_d32(DSD[index],diameter)
 
 # Log the required values for the data, viscosity and Modes
-properties_fix, fits_fix, indices = prep_data(properties,fits)
-del fits_fix, indices
+properties_fix, _, _ = prep_data(properties,fits)
 
 # Build the large new input properties array
 diameter_fix = np.log(diameter.copy()) #diameters
@@ -72,8 +71,8 @@ diameter_tiled = np.tile(diameter_fix, (len(properties), 1))
 combined = np.hstack((props_repeated, diameter_tiled))
 
 # Build the new indices list
-indices_f = combined[:,0].copy() # copy all ID values
-indices_f = indices_f.reshape(-1, 1)
+indices_DSD = combined[:,0].copy() # copy all ID values
+indices_DSD = indices_DSD.reshape(-1, 1)
 
 # Build the large output array
 DSD = np.asarray(DSD)
@@ -81,8 +80,8 @@ DSD_flat = DSD.reshape(-1, 1)
 DSD_flat = DSD_flat/100
 
 # Scale the arrays
-scaler_X_f, X_f_scaled = scale(combined[:,1:9])
-scaler_y_f, y_f_scaled = scale(DSD_flat)
+scaler_X_DSD, X_DSD_scaled = scale(combined[:,1:9])
+scaler_y_DSD, y_DSD_scaled = scale(DSD_flat)
 
 del DSD_flat, diameter_fix, properties_fix, props_repeated, diameter_tiled, combined
 
@@ -90,34 +89,32 @@ del DSD_flat, diameter_fix, properties_fix, props_repeated, diameter_tiled, comb
 Undertake the model fitting
 """
 # split the data into test, train, and validate
-Xf_test, Xf_train, Xf_valid, yf_test, yf_train, yf_valid, idx_f_test, idx_f_train, idx_f_valid = split_data(X_f_scaled,y_f_scaled,indices_f,test_fraction,validate_to_train_ratio)
+XDSD_test, XDSD_train, XDSD_valid, yDSD_test, yDSD_train, yDSD_valid, idx_DSD_test, idx_DSD_train, idx_DSD_valid = split_data(X_DSD_scaled,y_DSD_scaled,indices_DSD,test_fraction,validate_to_train_ratio)
 
 # undertake optimisation of the model for the volume fraction
 print("Running DSD Optimisation")
-final_model_f, history_volfrac_final, combined_volfrac_history, volfrac_info = optimise_model(Xf_train,yf_train,Xf_valid,yf_valid,Xf_test,yf_test,space_model,'relu',n_runs,num_optimise,num_initial)
-final_model_f.save("DSD_model.keras")
+final_model_DSD, history_DSD_final, combined_DSD_history, DSD_info = optimise_model(XDSD_train,yDSD_train,XDSD_valid,yDSD_valid,XDSD_test,yDSD_test,space_model,'relu',n_runs,num_optimise,num_initial)
+final_model_DSD.save("DSD_model.keras")
 
 # Get predictions for each split
-yf_pred_train = final_model_f.predict(Xf_train)
-yf_pred_valid = final_model_f.predict(Xf_valid)
-yf_pred_test = final_model_f.predict(Xf_test)
-yf_pred_train, yf_pred_valid, yf_pred_test, yf_train, yf_valid, yf_test = inverse_scale(yf_pred_train,yf_pred_valid,yf_pred_test,yf_train,yf_valid,yf_test,scaler_y_f,log=False)
+yDSD_pred_train = final_model_DSD.predict(XDSD_train)
+yDSD_pred_valid = final_model_DSD.predict(XDSD_valid)
+yDSD_pred_test = final_model_DSD.predict(XDSD_test)
+yDSD_pred_train, yDSD_pred_valid, yDSD_pred_test, yDSD_train, yDSD_valid, yDSD_test = inverse_scale(yDSD_pred_train,yDSD_pred_valid,yDSD_pred_test,yDSD_train,yDSD_valid,yDSD_test,scaler_y_DSD,log=False)
 
 # plot the fraction output values
-plot_outputs(yf_train[:, 0],yf_valid[:, 0],yf_test[:, 0],yf_pred_train[:, 0],yf_pred_valid[:, 0],yf_pred_test[:, 0],idx_f_train,idx_f_valid,idx_f_test,r'Plot for DSD',log=False, maerun=True,save_path='plots/DSDfull.png')
+plot_outputs(yDSD_train[:, 0],yDSD_valid[:, 0],yDSD_test[:, 0],yDSD_pred_train[:, 0],yDSD_pred_valid[:, 0],yDSD_pred_test[:, 0],idx_DSD_train,idx_DSD_valid,idx_DSD_test,r'Plot for DSD',log=False, maerun=True,save_path='plots/DSDfull.png')
 
 # plot the training history graph
-plot_training_history(history_volfrac_final, title=r'DSD Model Training History',save_path='plots/DSD_model_training.png')
-plot_training_history(combined_volfrac_history, title=r'DSD Model Training History',save_path='plots/DSD_model_combined.png')
-
-final_model_f = load_model("DSD_model.keras")
+plot_training_history(history_DSD_final, title=r'DSD Model Training History',save_path='plots/DSD_model_training.png')
+plot_training_history(combined_DSD_history, title=r'DSD Model Training History',save_path='plots/DSD_model_combined.png')
 
 # Re-run on the whole input to get the DSDs
-y_f_pred = predict_and_inverse(final_model_f, X_f_scaled)
-y_f_pred = 100*y_f_pred
+y_DSD_pred = predict_and_inverse(final_model_DSD, X_DSD_scaled, scaler_y_DSD)
+y_DSD_pred = 100*y_DSD_pred
 
 # Change shape of the distributions to match original DSD
-DSD_predicted = y_f_pred.reshape(len(properties), len(diameter))
+DSD_predicted = y_DSD_pred.reshape(len(properties), len(diameter))
 
 # calculate the d32's of the data
 d32_pred=np.zeros(len(DSD))
